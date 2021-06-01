133df5e7452fa28e0d803c2dbdf55337050a2608
Gisela <giselacepeda91@gmail.com>
2021-06-01 19:43:20 +0200

    almost finished project

--- my-app/src/Components/Playlist/Playlist.js
+++ my-app/src/Components/Playlist/Playlist.js
@@ -1,15 +1,27 @@
 import React from 'react';
 // import ReactDOM from'react-dom';
 import './Playlist.css'
-import '../TrackList/TrackList.js'
+import TrackList from '../TrackList/TrackList.js'
 
 class Playlist extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(event){
+    this.props.onNameChange(event.target.value)
+  }
+
   render(){
     return(
       <div className="Playlist">
-        <input defaultValue={"New Playlist"}/>
-        {/* <TrackList /> */}
-        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+        <input defaultValue={"New Playlist"} onChange={this.handleNameChange}/>
+        <TrackList  tracks={this.props.playlistTracks}
+                    onRemove={this.props.onRemove}
+                    isRemoval={true}/>
+        <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
       </div>
     )
   }
--- my-app/src/Components/TrackList/TrackList.js
+++ my-app/src/Components/TrackList/TrackList.js
@@ -1,18 +1,20 @@
 import React from 'react';
 // import ReactDOM from'react-dom';
 import './TrackList.css'
-// import Track from '../Track/Track.js'
+import Track from '../Track/Track.js'
 
 class TrackList extends React.Component{
     render(){
         return(
         <div className="TrackList">
-            {/* {
+            {
                 this.props.tracks.map(track => {
                     return <Track track={track} 
-                    key={track.id} />
+                    key={track.id} 
+                    onAdd = {this.props.onAdd}
+                    onRemoval ={this.props.onRemove}/>
                 })
-            } */}
+            }
         </div>)
     }
 }
